1f7edb89bc6e4f9130b9839e1b3bdda41ca25252
Yifan Li <yifanli3001@gmail.com>
2021-04-13 10:52:50 -0400

    add template folder
    also add home file

--- missweb/UserView/urls.py
+++ missweb/UserView/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 
-from . import views
+# from . import views
+from .views import HomePageView
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', HomePageView.as_view(), name='home'),
 ]
