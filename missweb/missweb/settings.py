273577bfc37ede9a50a78901be9b770923aca96e
Yifan Li <yifanli3001@gmail.com>
2021-04-15 22:09:17 -0400

    rewrite template DIRS

--- missweb/missweb/settings.py
+++ missweb/missweb/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'missweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR.joinpath('templates'))], # new 
+        'DIRS': [BASE_DIR / 'templates' ], # new 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
