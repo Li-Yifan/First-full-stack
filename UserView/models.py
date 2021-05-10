462ffa95d9c54c49e50cb060cdefe5de3eb9014e
Yifan Li <yifanli3001@gmail.com>
2021-05-10 12:18:01 -0400

    ready bf user info

--- UserView/models.py
+++ UserView/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class UserInfo(models.Model):
-    first_name = models.TextField()
-    last_name = models.TextField()
-    email = models.TextField()
+
 
     
